d06d19b26432c99221e1dea5f72b55742d0c632c
CaydenKeene <134308637+CaydenKeene@users.noreply.github.com>
2023-10-30 18:22:56 -0400

    added decoder function

--- lab6jd.py
+++ lab6jd.py
@@ -6,6 +6,20 @@ def encoder(password):
         x = x + str(int(i)+3)
     return x
 
+# decoder function, author: Cayden Keene
+def decoder(encoded_password):
+    password = ""
+    for i in encoded_password:
+        if int(i) == 0:
+            password += "7"
+        elif int(i) == 1:
+            password += "8"
+        elif int(i) == 2:
+            password += "9"
+        else:
+            password += str(int(i) - 3)
+    return password
+
 # main function, author: Jeremiah Dados
 if __name__ == '__main__':
     while True:
@@ -21,6 +35,9 @@ if __name__ == '__main__':
             print("Your password has been encoded and stored!\n")
         elif c == 2:
             # decoder function call goes here
+            # decoder call (added by Cayden Keene)
+            p = decoder(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {p}.")
             continue
         elif c == 3:
             break
